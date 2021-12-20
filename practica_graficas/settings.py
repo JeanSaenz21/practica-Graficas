620644c5d08d7c05ce10ff734c49c82f5be0f753
Jean Saenz <jeansaenz2001@gmail.com>
2021-12-19 18:07:20 -0700

    first commit

--- practica_graficas/settings.py
+++ practica_graficas/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'practica_graficas.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.mysql",
-        'NAME': 'bts',
-        'USER': 'apps',
-        'PASSWORD': '#WorkTogether12!',
-        'HOST': 'dtv-docker-mss01.us.signintra.com',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
